65df40a91c3fe2e1e33caf24fd842f5ecd05c0f9
Zasldqwi <ubinzat@gmail.com>
2020-11-26 16:04:54 +0300

    Gross error contamination model eklendi

--- EgimMSE.R
+++ EgimMSE.R
@@ -1,6 +1,7 @@
 n1=10
 B=1000
 N=3000
+eps <- 0.1
 b <- numeric(B)
 bhatmean <- numeric(N)
 bhat <- 2
@@ -9,9 +10,12 @@ for (i in 1:N){
   x1 <- rnorm(n1)
   error <- rnorm(n1)
   error[1:(n1/10)] <- 10
+  error[1:(n1*eps)] <- (error[1:(n1*eps)]+3*sample(c(-1,1),size = (n1*eps),replace = T))/0.1
+  
+  
   y <- 2*x1+error   #b yi 2 aldik
   
-  #error[1:(n1/10)] <- 10
+  
   ols <- lm(y~x1)
   for (j in 1:B){
     indice <- sample(1:length(x1),replace = T)
