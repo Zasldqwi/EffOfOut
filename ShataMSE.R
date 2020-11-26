a18fc92d2f79d341fb49635e57a09fc3855dbde0
Zasldqwi <ubinzat@gmail.com>
2020-11-26 15:34:01 +0300

    Shata için Bootsrap MSE

--- ShataMSE.R
+++ ShataMSE.R
@@ -1,22 +1,25 @@
-alpha=0.05
-n1=20
-p1=0.1
-s.d2=4
-N=1000
-B=999
+
+n1=10
+B=1499
+N=10000
 b <- numeric(B)
 s=1/sqrt(n1)
-k=0
-m.result <- matrix(ncol=2,nrow=N)
+se_est <- numeric(N)
+
+
 cont_norm <- function(n, mu = 0,sd1 = 1, sd2 = 9, prob = 0.1) {
   
   s <- sample(c(sd1, sd2), n, replace = T, prob = c(1 - prob, prob))
   rnorm(n, mean = mu, sd = s)
 }
 
-se_est <- numeric(N)
+
 for (i in 1:N){
-  x1 <- cont_norm(n = n1,prob = p1,sd1 = 1,sd2 = s.d2)
+  
+  if(i%/%(N/10)==i/(N/10))print(paste("%",i/(N/100),sep = ""))
+  x1 <- rnorm(n1)
+  # x1[1:(n1/10)] <- 10
+  x1 <- cont_norm(n = n1,prob = 0.1,sd1 = 1,sd2 = 25)
   
   for (j in 1:B){
     indice <- sample(1:length(x1),replace = T)
@@ -24,4 +27,6 @@ for (i in 1:N){
   }
   se_est [i]<- sd(b)
 }
-sum((se_est-s)^2)/N #MSE kestirimi
+
+mse <- sum((se_est-s)^2/N)
+mse
