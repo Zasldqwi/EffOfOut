b286a92544d35fffaa238d39d5ac838a44c5992a
Zasldqwi <ubinzat@gmail.com>
2020-11-26 15:30:40 +0300

    Ortalama için bootstrap MSE

---
+++ OrtalamaMSE.R
@@ -0,0 +1,32 @@
+
+n1=10
+B=1499
+N=10000
+b <- numeric(B)
+s=0
+mean.mse <- numeric(N)
+
+
+cont_norm <- function(n, mu = 0,sd1 = 1, sd2 = 9, prob = 0.1) {
+  
+  s <- sample(c(sd1, sd2), n, replace = T, prob = c(1 - prob, prob))
+  rnorm(n, mean = mu, sd = s)
+}
+
+
+for (i in 1:N){
+  
+  if(i%/%(N/10)==i/(N/10))print(paste("%",i/(N/100),sep = ""))
+  
+  # x1 <- rnorm(n1)
+  # x1[1:(n1/10)] <- 10
+  x1 <- cont_norm(n = n1,prob = 0.1,sd1 = 1,sd2 = 25 )
+  for (j in 1:B){
+    indice <- sample(1:length(x1),replace = T)
+    b[j]=mean(x1[indice])
+  }
+  mean.mse [i]<- mean(b)
+}
+
+mse <- sum((mean.mse-s)^2/N)
+mse
